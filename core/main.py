1649e153445c39a01c516ca532b563340fe67e6c
killman03 <tg3381234@gmail.com>
2024-03-30 13:26:33 +0600

    Change 30.03. Fir errors

--- core/main.py
+++ core/main.py
@@ -1,12 +1,15 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
 from aiogram.types import Message
 from core.handlers.basic import get_start
 import asyncio
 import logging
 from core.settings import settings
+from core.handlers.commands import set_commands
 
 
 async def on_startup(bot: Bot):
+    await set_commands(bot)
     await bot.send_message(settings.bots.ADMIN_ID, text='Bot is running')
 
 
@@ -17,7 +20,10 @@ async def start():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message).'
                         )
-    bot = Bot(token=settings.bots.BOT_TOKEN, parse_mode='HTML')
+    bot = Bot(
+    token=settings.bots.BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode="HTML"),
+)
 
     dp = Dispatcher()
     dp.startup.register(on_startup)
